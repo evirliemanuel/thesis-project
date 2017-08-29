8d8fdf4edf127157c200fd03c4ee8ee0f0d7ad80
inteliedoit <lieverandiver@gmail.com>
2017-08-29 22:52:39 +0800

    Create the Login Activity and services.

---
+++ app/src/main/java/com/lieverandiver/thesisproject/Clazz_QuizActivity.java
@@ -0,0 +1,13 @@
+package com.lieverandiver.thesisproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Clazz_QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clazz_quiz);
+    }
+}
---
+++ app/src/main/java/com/lieverandiver/thesisproject/fragment/LoginFragment.java
@@ -0,0 +1,24 @@
+package com.lieverandiver.thesisproject.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lieverandiver.thesisproject.R;
+
+/**
+ * Created by Verlie on 8/24/2017.
+ */
+
+public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+}
