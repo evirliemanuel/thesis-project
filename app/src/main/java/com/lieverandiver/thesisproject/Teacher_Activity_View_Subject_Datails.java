e9832070d2c5846a4ba7b22341927f1317d8c412
=inteliedoit <lieverandiver@gmail.com>
2017-09-04 10:49:10 +0800

    epic

--- app/src/main/java/com/lieverandiver/thesisproject/Teacher_Activity_View_Subject_Datails.java
+++ app/src/main/java/com/lieverandiver/thesisproject/Teacher_Activity_View_Subject_Datails.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import android.widget.Toast;
 
